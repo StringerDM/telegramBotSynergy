bc2ab8510be811c3ec013273ee6a4083ee4420ce
StringerDM <103386227+StringerDM@users.noreply.github.com>
2023-03-27 16:29:19 +0300

    lesson4 get and send image

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,7 +1,8 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
@@ -9,15 +10,40 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
         System.out.println(message.getText());
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Your Message: " + message.getText());
+
         try {
-            execute(sendMessage);
+            //1. Продолжая работу над ботом из предыдущих занятий, добавьте боту возможность отправлять сообщения
+            sendAnswer(update, "Your Message: " + message.getText());
+            //2. Реализуйте возможность получения изображения от пользователя, а также его отправку пользователю обратно
+            String path = getAndSavePhoto(update);
+            sendBackPhoto(update, path);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private void sendAnswer(Update update, String message) throws TelegramApiException {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId().toString());
+        sendMessage.setText(message);
+        execute(sendMessage);
+    }
+
+    private String getAndSavePhoto(Update update) throws TelegramApiException {
+        PhotoSize photo = update.getMessage().getPhoto().get(0);
+        GetFile getFile = new GetFile(photo.getFileId());
+        File file = execute(getFile);
+        String path = photo.getFileId() + ".jpeg";
+        downloadFile(file, new java.io.File(path));
+        return path;
+    }
+
+    private void sendBackPhoto(Update update, String path) throws TelegramApiException {
+        InputFile photo = new InputFile(new java.io.File(path));
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(update.getMessage().getChatId().toString());
+        sendPhoto.setPhoto(photo);
+        execute(sendPhoto);
     }
 
     public String getBotUsername() {
